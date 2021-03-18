8f539a50c8f1b2e4cccefa7e3903dbd57ad9cf62
BrunoCereda9979 <bruno9979@gmail.com>
2021-03-18 14:51:35 -0300

    Refactored the 'operations.js' file to not close connection with database.

--- server/routes/operations.js
+++ server/routes/operations.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
-// Connection to Dabatabase
+// New Connection
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,45 +10,36 @@ let connection = mysql.createConnection({
     database: 'budget-app-db',
 });
 
+// Connect to Database
+connection.connect((err) => {
+    if (err) throw Error('Cant connect to dabase');
+    console.log('Connected to database...');
+});
+
 // Get the first 10 operations
 router.get('', (req, res) => {
-    connection.connect((err) => {
-        if (err) {
-            res.json({
-                'status': 500,
-                'message': 'There is an error with the database connection' 
-            })
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('---Cant close connection to database' + err);
-                } 
-            })
-        }
-        else {
-            connection.query('SELECT * FROM `operations` LIMIT 0, 10', (errors, results, fields) => {
-                if (errors) {
-                    res.status(500).json({
-                        'status': 500,
-                        'message': 'There is an error with the query' 
-                    })
-                }
-                else {
-                    res.status(200).json({
-                        'status': 200,
-                        'message': {
-                            results
-                        }
-                    })
-                }   
-                connection.end((err) => {
-                    if (err) {
-                        console.log('Cant close connection to database' + err);
-                    }   
-                });
-            });
-        }
-    });
+    if (connection.state === 'authenticated') {
+        connection.query('SELECT * FROM `operations` LIMIT 0, 10', (err, result) => {
+            if (err) {
+                res.status(500).json({
+                    'message': 'Could not query data from the database'
+                })
+            }
+            else {
+                res.status(200).json({
+                    'status': 200,
+                    'message': {
+                        result
+                    }
+                });             
+            }
+        });
+    }
+    else {
+        res.status(500).json({
+            'message': 'Sorry, there is an error with the server'
+        })
+    }
 });
 
 // Post a new operation
@@ -64,42 +55,20 @@ router.post('', (req, res) => {
     const sql = "INSERT INTO `operations` (`operation_desc`, `operation_amount`, `operation_date`, `operation_type`) VALUES (?, ?, ?, ?)";
     let values = [newOperation.description, newOperation.amount, newOperation.date, newOperation.type];
 
-    connection.connect((err) => {
-        if (err) {
-            res.json({
+    connection.query(sql, values, (errors, results, fields) => {
+        if (errors) {
+            res.status(500).json({
                 'status': 500,
-                'message': 'There is an error with the database connection' 
-            })
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                } 
-            })
+                'message': 'There is an error with the query' 
+            });
         }
         else {
-            connection.query(sql, values, (errors, results, fields) => {
-                if (errors) {
-                    res.status(500).json({
-                        'status': 500,
-                        'message': 'There is an error with the query' 
-                    })
+            res.status(200).json({
+                'status': 200,
+                'message': {
+                    results
                 }
-                else {
-                    res.status(200).json({
-                        'status': 200,
-                        'message': {
-                            results
-                        }
-                    })
-                }
-            });
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                }   
-            });
+            })
         }
     });
 });
@@ -109,41 +78,19 @@ router.delete('', (req, res) => {
     const sql = "DELETE FROM `operations` WHERE operation_id = ?";
     let value =  req.body.operationId;
 
-    connection.connect((err) => {
-        if (err) {
-            res.json({
+    connection.query(sql, value, (errors, results, fields) => {
+        if (errors) {
+            res.status(500).json({
                 'status': 500,
-                'message': 'There is an error with the database connection' 
-            })
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                } 
-            })
+                'message': 'There is an error with the query' 
+            });
         }
         else {
-            connection.query(sql, value, (errors, results, fields) => {
-                if (errors) {
-                    res.status(500).json({
-                        'status': 500,
-                        'message': 'There is an error with the query' 
-                    })
+            res.status(200).json({
+                'status': 200,
+                'message': {
+                    results
                 }
-                else {
-                    res.status(200).json({
-                        'status': 200,
-                        'message': {
-                            results
-                        }
-                    })
-                }
-            });
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                }   
             });
         }
     });    
@@ -155,42 +102,19 @@ router.put('', (req, res) => {
     let sql = "UPDATE `operations` SET operation_desc = ?, operation_amount = ?, operation_date = ?, operation_type = ? WHERE operation_id = ?";
     let values = [newOperation.operationDesc, newOperation.operationAmount, newOperation.operationDate, newOperation.operationType, newOperation.operationId];
 
-    connection.connect((err) => {
-        if (err) {
-            res.json({
+    connection.query(sql, values, (errors, results, fields) => {
+        if (errors) {
+            res.status(500).json({
                 'status': 500,
-                'message': 'There is an error with the database connection' 
-            })
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                } 
-            })
+                'message': 'There is an error with the query' 
+            });
         }
         else {
-            connection.query(sql, values, (errors, results, fields) => {
-                if (errors) {
-                    res.status(500).json({
-                        'status': 500,
-                        'message': 'There is an error with the query' 
-                    })
-                    console.log(errors);
+            res.status(200).json({
+                'status': 200,
+                'message': {
+                    results
                 }
-                else {
-                    res.status(200).json({
-                        'status': 200,
-                        'message': {
-                            results
-                        }
-                    })
-                }
-            });
-
-            connection.end((err) => {
-                if (err) {
-                    console.log('Cant close connection to database' + err);
-                }   
             });
         }
     });
