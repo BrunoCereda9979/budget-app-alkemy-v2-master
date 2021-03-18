47a0cea4c65b07801d9372ea2c5cf798f6980345
BrunoCereda9979 <bruno9979@gmail.com>
2021-03-18 12:29:02 -0300

    Deleted some useless files and fetched data from the API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [allOperations, setAllOperations] = useState();
+
+  // Get all operations when component rendered
+  useEffect(() => {
+    fetch("/api/operations")
+      .then((res) => res.json())
+      .then((data) => setAllOperations(data.message))
+      .catch((err) => console.log(err))
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
